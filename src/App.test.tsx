91bc66d08b7beeeff72fafa47a4b3c409a36ad8e
Jonathan Newman <jbnewm@gmail.com>
2020-02-28 19:55:02 -0600

    fixed smoke test

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -2,8 +2,8 @@ import React from 'react';
 import { render } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders header with my name', () => {
   const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const nameDisplay = getByText(/Jonathan Newman/i);
+  expect(nameDisplay).toBeInTheDocument();
 });
