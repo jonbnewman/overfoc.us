201c7463dffb5babbc141d34597bda7065fd425c
Jonathan Newman <jbnewm@gmail.com>
2020-02-29 08:44:49 -0600

    fixed test issues

---
+++ src/App.test.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { render } from "@testing-library/react";
+import "@testing-library/jest-dom/extend-expect";
+import App from "../App";
+
+test("renders header with my name", () => {
+  const { getByText } = render(<App />);
+  const nameDisplay = getByText(/Jonathan Newman/i);
+  expect(nameDisplay).toBeInTheDocument();
+});
---
+++ src/components/Footer/GithubLink.ts
@@ -0,0 +1,10 @@
+import styled from "styled-components/macro";
+import { Link } from "@material-ui/core";
+
+export default styled(Link)`
+  display: block;
+  && {
+    font-size: 0.9rem;
+    margin-top: 0.25rem;
+  }
+`;
