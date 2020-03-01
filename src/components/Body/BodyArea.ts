e93ebb8bfb3578e80d65a778a33f0f8bef826a9a
Jonathan Newman <jbnewm@gmail.com>
2020-03-01 08:02:58 -0600

    fixed build/linter errors

--- src/components/Body/BodyArea.ts
+++ src/components/Body/BodyArea.ts
@@ -1,9 +1,7 @@
 import styled from "styled-components/macro";
 import Container from "@material-ui/core/Container";
 import { constraintSize } from "theme";
-import { constraint } from "components/Constraint";
 
 export default styled(Container).attrs({ maxWidth: constraintSize, component: "main" })`
   grid-area: body;
-  ${constraint}
 `;
--- src/pages/ProjectList/ProjectDisplay/CardActionArea.ts
+++
@@ -1,8 +0,0 @@
-import styled from "styled-components";
-import CardActionArea from "@material-ui/core/CardActionArea";
-import { grey, indigo, green, blue, lightBlue } from "@material-ui/core/colors";
-
-export default styled(CardActionArea)`
-  && {
-  }
-`;
