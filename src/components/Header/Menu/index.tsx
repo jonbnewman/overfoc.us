2fdb61a17c7a748a7b9486c81d0853c53b40468f
Jonathan Newman <jbnewm@gmail.com>
2020-03-01 14:23:15 -0600

    added home icon

--- src/components/Header/Menu/index.tsx
+++ src/components/Header/Menu/index.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { useStore } from "mobx-store-provider";
+import HomeIcon from "@material-ui/icons/Home";
 import { IStore } from "state/Store";
 import Container from "./Container";
 import MenuLink from "./MenuLink";
@@ -8,7 +9,9 @@ function Menu() {
   const store: IStore = useStore();
   return (
     <Container>
-      <MenuLink to="/">Home</MenuLink>
+      <MenuLink startIcon={<HomeIcon />} to="/">
+        Home
+      </MenuLink>
       {store.project_status_types.map((statusType: any) => (
         <MenuLink key={statusType.status} to={statusType.path}>
           {statusType.label}
