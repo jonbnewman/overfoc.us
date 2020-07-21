44a85cd179a7e1a8c90ac72be21f02b5ea0b54b0
jonbnewman <jbnewm@gmail.com>
2020-07-21 19:19:14 +0000

    Deploy to Github Pages

---
+++ precache-manifest.a101ecac9098b92dd04200f8a33193d3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "31197f16662e96afc9893e27e2d9b102",
+    "url": "/index.html"
+  },
+  {
+    "revision": "30cbbe0f0526d56c4de2",
+    "url": "/static/css/2.764ccc25.chunk.css"
+  },
+  {
+    "revision": "30cbbe0f0526d56c4de2",
+    "url": "/static/js/2.7af102aa.chunk.js"
+  },
+  {
+    "revision": "ddd2add7f8d9ffedda1c0e5ba30e16b5",
+    "url": "/static/js/2.7af102aa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3f7b5db99c6acfb0acc0",
+    "url": "/static/js/main.85f44324.chunk.js"
+  },
+  {
+    "revision": "916a6ee84cfb15d1217c",
+    "url": "/static/js/runtime-main.f87ccd16.js"
+  },
+  {
+    "revision": "1d53ff0d71657cd260b5da65090f868d",
+    "url": "/static/media/contents.1d53ff0d.md"
+  }
+]);
---
+++ static/media/contents.1d53ff0d.md
@@ -0,0 +1,13 @@
+# Introduction
+
+Welcome to the projects page for Jonathan Newman.
+
+On this site you will find links to various active and inactive projects related to web development, applications, and technology in general.
+
+This is one part personal record, and one part demo area.
+
+---
+
+If you are an employer or have a project of interest you may also want to see:
+
+**📄 [My work resume](https://docs.google.com/document/d/1JJNVaHSsKgTB7Jy_qgh52HjRdxAOJHTXpKJenqbEMw4)**
